3b63355d0829f8058774e10391f925a00d555b08
bbangjae <thebyunjae@naver.com>
2025-06-23 23:38:49 +0900

    feat(repository): MemberRepository 인터페이스 생성

---
+++ src/main/java/com/example/spring_member_management/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.example.spring_member_management.repository;
+
+import com.example.spring_member_management.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
