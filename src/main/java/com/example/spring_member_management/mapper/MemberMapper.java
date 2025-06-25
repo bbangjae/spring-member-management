1f68953db92e965c8a873387bfeb58702e05aa88
bbangjae <thebyunjae@naver.com>
2025-06-25 22:52:10 +0900

    feat(mapper): MemberMapper MyBatis 어노테이션 기반 구현 추가

---
+++ src/main/java/com/example/spring_member_management/mapper/MemberMapper.java
@@ -0,0 +1,36 @@
+package com.example.spring_member_management.mapper;
+
+import com.example.spring_member_management.domain.Member;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Mapper
+public interface MemberMapper {
+
+    @Insert("INSERT INTO member(member_name) VALUES(#{memberName})")
+    @Options(useGeneratedKeys = true, keyProperty = "memberId")
+    void save(Member member);
+
+    @Select("SELECT member_id, member_name FROM member WHERE member_id = #{memberId}")
+    @Results({
+            @Result(property = "memberId", column = "member_id"),
+            @Result(property = "memberName", column = "member_name")
+    })
+    Optional<Member> findById(@Param("memberId") Long id);
+
+    @Select("SELECT member_id, member_name FROM member WHERE member_name = #{memberName}")
+    @Results({
+            @Result(property = "memberId", column = "member_id"),
+            @Result(property = "memberName", column = "member_name")
+    })
+    Optional<Member> findByName(@Param("memberName") String name);
+
+    @Select("SELECT member_id, member_name FROM member")
+    @Results({
+            @Result(property = "memberId", column = "member_id"),
+            @Result(property = "memberName", column = "member_name")
+    })
+    List<Member> findAll();
+}
