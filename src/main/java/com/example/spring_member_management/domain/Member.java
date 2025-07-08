1d6549c7b83c2061569be0766da099ad3ac2905b
bbangjae <120161896+bbangjae@users.noreply.github.com>
2025-07-08 10:45:11 +0900

    refactor(member): 도메인 setter 메서드 제거 (#20)

--- src/main/java/com/example/spring_member_management/domain/Member.java
+++ src/main/java/com/example/spring_member_management/domain/Member.java
@@ -5,11 +5,19 @@ import lombok.Setter;
 
 @Getter
 public class Member {
-    @Setter
     private Long memberId;
     private String memberName;
 
     public Member(String memberName) {
+        this(null, memberName);
+    }
+
+    public Member(Long memberId, String memberName) {
+        this.memberId = memberId;
         this.memberName = memberName;
+    }
+
+    public void updateName(String newName) {
+        this.memberName = newName;
     }
 }
