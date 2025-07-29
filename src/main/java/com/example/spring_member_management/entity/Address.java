45b6f8a1a7c7d050dffc76c8645e8212b523bcf9
bbangjae <thebyunjae@naver.com>
2025-07-29 23:48:13 +0900

    feat(entity): Address 임베디드 구현

---
+++ src/main/java/com/example/spring_member_management/entity/Address.java
@@ -0,0 +1,36 @@
+package com.example.spring_member_management.entity;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Address {
+
+    private String street;
+    private String city;
+    private String zipcode;
+
+    public Address(String street, String city, String zipcode) {
+        this.street = street;
+        this.city = city;
+        this.zipcode = zipcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(street, address.street) && Objects.equals(city, address.city) && Objects.equals(zipcode, address.zipcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(street, city, zipcode);
+    }
+}
