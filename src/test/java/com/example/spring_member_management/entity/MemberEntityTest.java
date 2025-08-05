18a73d11d8dba9d0dade8edbf78a9bd78b1a9d3f
bbangjae <120161896+bbangjae@users.noreply.github.com>
2025-08-05 22:09:22 +0900

    test(member): 멤버 엔티티 테스트 코드 작성  (#34)
    * test(member): 멤버 주소 저장 및 조회 테스트 추가
    
    * test(member): 멤버 락커 배정 성공 테스트 코드 추가

---
+++ src/test/java/com/example/spring_member_management/entity/MemberEntityTest.java
@@ -0,0 +1,54 @@
+package com.example.spring_member_management.entity;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class MemberEntityTest {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Test
+    void 멤버_주소_생성_성공() {
+        // given
+        Address address = new Address("서울", "신림", "1234");
+        Member member = new Member("jae", address);
+
+        em.persist(member);
+        em.flush();
+        em.clear();
+        // when
+        Member foundMember = em.find(Member.class, member.getId());
+
+        // then
+        assertThat(foundMember.getName()).isEqualTo("jae");
+        assertThat(foundMember.getAddress()).isEqualTo(address);
+    }
+
+    @Test
+    void 멤버_락커_배정_성공() {
+        //given
+        Address address = new Address("서울", "신림", "1234");
+        Member member = new Member("jae", address);
+        Locker locker = new Locker("L1");
+        member.assignLocker(locker);
+
+        em.persist(locker);
+        em.persist(member);
+        em.flush();
+        em.clear();
+
+        //when
+        Member foundMember = em.find(Member.class, member.getId());
+
+        //then
+        assertThat(foundMember.getLocker()).isEqualTo(locker);
+    }
+}
