263027bc8415551b8e7d32ba0187cc6a5c8bcc78
bbangjae <thebyunjae@naver.com>
2025-08-06 22:41:59 +0900

    test(base-entity): getter 추가

--- src/main/java/com/example/spring_member_management/entity/BaseEntity.java
+++ src/main/java/com/example/spring_member_management/entity/BaseEntity.java
@@ -1,6 +1,7 @@
 package com.example.spring_member_management.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@Getter
 public class BaseEntity {
 
     @CreatedBy
