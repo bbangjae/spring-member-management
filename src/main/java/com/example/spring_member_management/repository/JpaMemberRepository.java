3ea5b8d4dc7368946b6bd433ea5f355d0e2786c8
bbangjae <thebyunjae@naver.com>
2025-08-09 23:55:06 +0900

    feat(jpa-repository): 이름 조회(findByName)와 팀 페치(findAllWithTeam) 메서드 도입(N+1 방지) 코드 추가

--- src/main/java/com/example/spring_member_management/repository/JpaMemberRepository.java
+++ src/main/java/com/example/spring_member_management/repository/JpaMemberRepository.java
@@ -1,7 +1,18 @@
 package com.example.spring_member_management.repository;
 
 import com.example.spring_member_management.entity.Member;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface JpaMemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByName(String name);
+
+    @EntityGraph(attributePaths = "team")
+    @Query("SELECT m FROM Member m")
+    List<Member> findAllWithTeam();
 }
