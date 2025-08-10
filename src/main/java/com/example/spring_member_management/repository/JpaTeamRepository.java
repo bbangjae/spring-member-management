8c015eee9a2aebc381151b8decfcb299dcdffcfa
bbangjae <thebyunjae@naver.com>
2025-08-10 15:35:55 +0900

    feat(jpa-repository): 스프링 데이터 JPA 팀 Repository 코드 추가

---
+++ src/main/java/com/example/spring_member_management/repository/JpaTeamRepository.java
@@ -0,0 +1,8 @@
+package com.example.spring_member_management.repository;
+
+import com.example.spring_member_management.entity.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaTeamRepository extends JpaRepository<Team, Long> {
+
+}
