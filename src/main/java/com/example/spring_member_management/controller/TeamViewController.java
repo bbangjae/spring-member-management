8301252b5a76d91056e5b4d9f2b2f2b5162a000b
bbangjae <thebyunjae@naver.com>
2025-08-10 23:39:11 +0900

    feat(jpa-controller): 팀 생성 view controller 구현

---
+++ src/main/java/com/example/spring_member_management/controller/TeamViewController.java
@@ -0,0 +1,15 @@
+package com.example.spring_member_management.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/teams")
+public class TeamViewController {
+
+    @GetMapping("/new")
+    public String showCreateTeamForm() {
+        return "teams/createteam--form";
+    }
+}
