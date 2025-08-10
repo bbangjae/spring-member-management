35b5a4030e658462259673d2342be83e16b88b1e
bbangjae <120161896+bbangjae@users.noreply.github.com>
2025-08-10 15:40:26 +0900

    feat(jpa-dto): 회원 요청/응답 DTO 추가 (MemberWithTeamResponseDto, MemberWithAddressRequestDto) (#42)

---
+++ src/main/java/com/example/spring_member_management/dto/MemberWithAddressRequestDto.java
@@ -0,0 +1,43 @@
+package com.example.spring_member_management.dto;
+
+import com.example.spring_member_management.entity.Address;
+import com.example.spring_member_management.entity.Member;
+import com.example.spring_member_management.entity.Team;
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberWithAddressRequestDto {
+
+    private String memberName;
+    private Long teamId;
+    private AddressDto address;
+
+    public Member toEntity(Team team) {
+        return Member.builder()
+                .memberName(this.memberName)
+                .address(this.address.toAddressEntity())
+                .team(team)
+                .build();
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class AddressDto {
+        private String street;
+        private String city;
+        private String zipcode;
+
+        public Address toAddressEntity() {
+            return Address.builder()
+                    .street(this.street)
+                    .city(this.city)
+                    .zipcode(this.zipcode)
+                    .build();
+        }
+    }
+}
