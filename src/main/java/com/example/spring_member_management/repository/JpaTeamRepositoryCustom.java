770c0a9131dd5545909e2390ee521c25f9054555
bbangjae <thebyunjae@naver.com>
2025-08-17 05:18:38 +0900

    feat(QueryDSL, Impl): 팀 디테일 RepositoryCustom, RepositoryImpl 코드 추가

--- src/main/java/com/example/spring_member_management/repository/JpaTeamRepositoryCustom.java
+++ src/main/java/com/example/spring_member_management/repository/JpaTeamRepositoryCustom.java
@@ -1,9 +1,13 @@
 package com.example.spring_member_management.repository;
 
 import com.example.spring_member_management.dto.TeamWithMemberCountDto;
+import com.example.spring_member_management.entity.Team;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface JpaTeamRepositoryCustom {
     List<TeamWithMemberCountDto> findAllTeamsWithMemberCount();
+
+    Optional<Team> findTeamDetailById(Long teamId);
 }
--- src/main/java/com/example/spring_member_management/repository/JpaTeamRepositoryImpl.java
+++ src/main/java/com/example/spring_member_management/repository/JpaTeamRepositoryImpl.java
@@ -3,11 +3,17 @@ package com.example.spring_member_management.repository;
 import com.example.spring_member_management.dto.TeamWithMemberCountDto;
 import static com.example.spring_member_management.entity.QTeam.team;
 import static com.example.spring_member_management.entity.QMember.member;
+
+import com.example.spring_member_management.entity.QLocker;
+import com.example.spring_member_management.entity.QMember;
+import com.example.spring_member_management.entity.QTeam;
+import com.example.spring_member_management.entity.Team;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class JpaTeamRepositoryImpl implements JpaTeamRepositoryCustom {
@@ -25,5 +31,21 @@ public class JpaTeamRepositoryImpl implements JpaTeamRepositoryCustom {
                 .leftJoin(team.members, member)
                 .groupBy(team.id, team.name)
                 .fetch();
+    }
+
+    @Override
+    public Optional<Team> findTeamDetailById(Long teamId) {
+        QTeam team = new QTeam("team");
+        QMember member = new QMember("member");
+        QLocker locker = new QLocker("locker");
+
+        return Optional.ofNullable(
+                jpaQueryFactory
+                        .selectFrom(team).distinct()
+                        .leftJoin(team.members, member).fetchJoin()
+                        .leftJoin(member.locker, locker).fetchJoin()
+                        .where(team.id.eq(teamId))
+                        .fetchOne()
+        );
     }
 }
