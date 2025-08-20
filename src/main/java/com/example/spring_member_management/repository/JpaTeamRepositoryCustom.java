446a00b4810b4b2204eeb3817b0d09d4af0ab534
bbangjae <thebyunjae@naver.com>
2025-08-20 22:02:07 +0900

    feat(QueryDSL, back): 팀 목록 조회에 멤버 수 계산용 페이징 쿼리 추가

--- src/main/java/com/example/spring_member_management/repository/JpaTeamRepositoryCustom.java
+++ src/main/java/com/example/spring_member_management/repository/JpaTeamRepositoryCustom.java
@@ -2,6 +2,8 @@ package com.example.spring_member_management.repository;
 
 import com.example.spring_member_management.dto.TeamWithMemberCountDto;
 import com.example.spring_member_management.entity.Team;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +12,6 @@ public interface JpaTeamRepositoryCustom {
     List<TeamWithMemberCountDto> findAllTeamsWithMemberCount();
 
     Optional<Team> findTeamDetailById(Long teamId);
+
+    Page<Team> findTeamsWithMemberCount(Pageable pageable);
 }
