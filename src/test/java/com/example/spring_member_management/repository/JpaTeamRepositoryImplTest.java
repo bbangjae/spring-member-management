e3f1f54c7bb80879a3e602098f1ea85f59c78c0b
bbangjae <thebyunjae@naver.com>
2025-08-21 23:49:18 +0900

    test(QueryDSL, back): 팀 목록 조회 페이징 테스트 코드 작성

--- src/test/java/com/example/spring_member_management/repository/JpaTeamRepositoryImplTest.java
+++ src/test/java/com/example/spring_member_management/repository/JpaTeamRepositoryImplTest.java
@@ -8,6 +8,8 @@ import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -57,5 +59,37 @@ class JpaTeamRepositoryImplTest {
                         tuple("홍보팀", 0L),
                         tuple("개발팀", 1L)
                 );
+    }
+
+    @Test
+    void 팀_목록_조회_페이징_성공() {
+        //given
+        for (int i = 1; i < 12; i++) {
+            Team team = new Team("Team" + i);
+            jpaTeamRepository.save(team);
+        }
+
+        Team team12 =  new Team("Team12");
+        Member member = new Member("Member1", null, team12);
+        Member member2 = new Member("Member2", null, team12);
+
+        em.persist(team12);
+        em.persist(member);
+        em.persist(member2);
+
+        em.flush();
+        em.clear();
+        //when
+        PageRequest pageRequest = PageRequest.of(1, 10);
+        Page<Team> result = jpaTeamRepository.findTeamsWithMemberCount(pageRequest);
+
+        //then
+        assertThat(result.getContent())
+                .filteredOn(team -> team.getName().equals("Team12"))
+                .extracting(Team::getName, team -> team.getMembers().size())
+                .containsExactly(tuple("Team12", 2));
+
+        assertThat(result.getTotalElements()).isEqualTo(12);
+        assertThat(result.getTotalPages()).isEqualTo(2);
     }
 }
